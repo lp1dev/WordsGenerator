f34db68e6a0278b70aebb51dc7eff6124eca12dc
lupin <mail@lp1.eu>
2016-12-20 16:32:26 +0100

    added web service with cherrypy

--- main.py
+++ main.py
@@ -1,51 +1,13 @@
 #!/bin/python
 
-from sys import exit
+import cherrypy
 from random import randrange
-from rules import wordRules
-
-vowels = "aeiouy"
-consonants = "bcdfghjklmnpqrstvwxyz"
-letters = vowels + consonants
-
-def     usage():
-  return 0
-
-def num_vowels(word):
-  total = 0
-  for letter in word:
-    if letter in vowels:
-      total += 1
-  return total
-
-def num_consonants(word):
-  total = 0
-  for letter in word:
-    if letter in consonants:
-      total += 1
-  return total
-
-def is_rules_compliant(length, word, new_letter):
-  for rule in wordRules:
-    try:
-      if not eval(rule):
-        return False
-    except SyntaxError as e:
-      exit(e)
-  return True
-
-def generate_word(length, word=""):
-  if len(word) == length:
-    return word
-  new_letter = letters[randrange(2, len(letters))]
-  if is_rules_compliant(length, word, new_letter):
-    word += new_letter
-  return generate_word(length, word)
+from wordgen import WordGen
 
 def	main():
-  wordlength = randrange(2, len(letters))
-  print(generate_word(wordlength))
-  return 0
+  wordlength = randrange(2, 26)
+  cherrypy.server.socket_host = "0.0.0.0"
+  cherrypy.quickstart(WordGen())
 
 if __name__ == '__main__':
   main()
